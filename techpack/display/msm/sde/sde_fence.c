243f4fd5755dbe088276bed059fd3b1581ba249a
Park Ju Hyung <qkrwngud825@gmail.com>
2021-03-14 10:26:57 -0400

    techpack: display: add some bp hints to hot paths
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- techpack/display/msm/sde/sde_fence.c
+++ techpack/display/msm/sde/sde_fence.c
@@ -205,7 +205,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 
 	/* create fd */
 	fd = get_unused_fd_start_flags(1, 0);
-	if (fd < 0) {
+	if (unlikely(fd < 0)) {
 		dma_fence_put(&sde_fence->base);
 		goto exit;
 	}
