60fde78de0442710c99bb5c4802b5284dcd0fa17
Prythomn <107319458+Prythomn@users.noreply.github.com>
2024-12-25 17:01:16 +0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Добавляем данные из настроек
-source ../settings.sh
+. ../settings.sh
 
 # Начало отсчета времени выполнения скрипта
 start_time=$(date +%s)
@@ -10,17 +10,17 @@ start_time=$(date +%s)
 rm -rf out
 
 # Основной каталог
-MAINPATH=/home/timisong # измените, если необходимо
+MAINPATH=$HOME # измените, если необходимо
 
 # Каталог ядра
-KERNEL_DIR=$MAINPATH/kernel
+KERNEL_DIR=$MAINPATH
 KERNEL_PATH=$KERNEL_DIR/kernel_xiaomi_sm8250
 
 git log $LAST..HEAD > ../changelog.txt
 BRANCH=$(git branch --show-current)
 
 # Каталоги компиляторов
-CLANG_DIR=$KERNEL_DIR/clang20
+CLANG_DIR=/lib/llvm-18/bin/clang
 ANDROID_PREBUILTS_GCC_ARM_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 ANDROID_PREBUILTS_GCC_AARCH64_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
@@ -35,23 +35,8 @@ check_and_clone() {
     fi
 }
 
-check_and_wget() {
-    local dir=$1
-    local repo=$2
-
-    if [ ! -d "$dir" ]; then
-        echo "Папка $dir не существует. Клонирование $repo."
-        mkdir $dir
-        cd $dir
-        wget $repo
-        tar -zxvf Clang-20.0.0git-20241222.tar.gz
-        rm -rf Clang-20.0.0git-20241222.tar.gz
-        cd ../kernel_xiaomi_sm8250
-    fi
-}
 
 # Клонирование инструментов компиляции, если они не существуют
-check_and_wget $CLANG_DIR https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20241222-release/Clang-20.0.0git-20241222.tar.gz
 check_and_clone $ANDROID_PREBUILTS_GCC_ARM_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 check_and_clone $ANDROID_PREBUILTS_GCC_AARCH64_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
@@ -90,9 +75,9 @@ export DTBPATH="$MAGIC_TIME_DIR/dtb"
 export DTBOPATH="$MAGIC_TIME_DIR/dtbo.img"
 export CROSS_COMPILE="aarch64-linux-gnu-"
 export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
-export KBUILD_BUILD_USER="TIMISONG"
-export KBUILD_BUILD_HOST="timisong-dev"
-export MODEL="alioth"
+export KBUILD_BUILD_USER="Olzhas"
+export KBUILD_BUILD_HOST="olzhas-nodev"
+export MODEL="munch"
 
 # Запись времени сборки
 MAGIC_BUILD_DATE=$(date '+%Y-%m-%d_%H-%M-%S')
@@ -102,7 +87,7 @@ output_dir=out
 
 # Конфигурация ядра
 make O="$output_dir" \
-            ${DEVICE}_defconfig \
+            munch_defconfig \
             vendor/xiaomi/sm8250-common.config
 
     # Компиляция ядра
@@ -138,40 +123,12 @@ if grep -q -E "Ошибка 2|Error 2" build.log; then
     cd "$KERNEL_PATH"
     echo "Ошибка: Сборка завершилась с ошибкой"
 
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendMessage \
-    -d chat_id="@magictimekernel" \
-    -d text="Ошибка в компиляции!" \
-    -d message_thread_id="38153"
-
-    curl -s -X POST "https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel" \
-    -F document=@"./build.log" \
-    -F message_thread_id="38153"
-
-    curl -s -X POST "https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel" \
-    -F document=@"../changelog.txt" \
-    -F message_thread_id="38153"
 else
     echo "Общее время выполнения: $elapsed_time секунд"
     # Перемещение в каталог MagicTime и создание архива
     cd "$MAGIC_TIME_DIR"
     7z a -mx9 MagicTime-$DEVICE-$MAGIC_BUILD_DATE.zip * -x!*.zip
-    
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendMessage \
-    -d chat_id="@magictimekernel" \
-    -d text="Компиляция завершилась успешно! Время выполнения: $elapsed_time секунд" \
-    -d message_thread_id="38153"
 
-    curl -s -X POST "https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel" \
-    -F document=@"./MagicTime-$DEVICE-$MAGIC_BUILD_DATE.zip" \
-    -F caption="MagicTime ${VERSION}${PREFIX}${BUILD} (${BUILD_TYPE}) branch: ${BRANCH}" \
-    -F message_thread_id="38153"
-    
-    curl -s -X POST "https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel" \
-    -F document=@"../changelog.txt" \
-    -F caption="Latest changes" \
-    -F message_thread_id="38153"
-
-    rm -rf MagicTime-$DEVICE-$MAGIC_BUILD_DATE.zip
 
     BUILD=$((BUILD + 1))
 
